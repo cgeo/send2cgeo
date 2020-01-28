172b0cc6ddbccba24ed14a467c53d1111a048a0e
Bernd <32555742+bekuno@users.noreply.github.com>
2020-01-28 22:58:46 +0100

    increase version number

--- send2cgeo.user.js
+++ send2cgeo.user.js
@@ -22,7 +22,7 @@
 // @downloadURL    https://github.com/cgeo/send2cgeo/raw/release/send2cgeo.user.js
 // @updateURL      https://github.com/cgeo/send2cgeo/raw/release/send2cgeo.user.js
 // @supportURL     https://github.com/cgeo/send2cgeo/issues
-// @version        0.47
+// @version        0.48
 // ==/UserScript==
 
 // Inserts javascript that will be called by the s2cgeo button. The closure
