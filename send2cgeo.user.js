7b921ad2bf19f1463d6257bec0ae3a1c2eecec14
Lars <gc@droescher.eu>
2020-02-20 16:20:49 +0100

    Raise version for release - Fix #90

--- send2cgeo.user.js
+++ send2cgeo.user.js
@@ -12,7 +12,7 @@
 // @downloadURL    https://github.com/cgeo/send2cgeo/raw/release/send2cgeo.user.js
 // @updateURL      https://github.com/cgeo/send2cgeo/raw/release/send2cgeo.user.js
 // @supportURL     https://github.com/cgeo/send2cgeo/issues
-// @version        0.48
+// @version        0.49
 // ==/UserScript==
 
 // Function that handles the actual sending
