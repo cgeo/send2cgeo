2f085c22aed8795472fa2fc6326b78970de1b4ef
Lars <gc@droescher.eu>
2020-12-02 11:51:07 +0100

    Burn in version code
    in preparation for release by merging `master` to `release`

--- send2cgeo.user.js
+++ send2cgeo.user.js
@@ -17,7 +17,7 @@
 // @downloadURL    https://github.com/cgeo/send2cgeo/raw/release/send2cgeo.user.js
 // @updateURL      https://github.com/cgeo/send2cgeo/raw/release/send2cgeo.user.js
 // @supportURL     https://github.com/cgeo/send2cgeo/issues
-// @version        2020.08.26
+// @version        2020.12.02
 // @grant          GM_setValue
 // @grant          GM_getValue
 // ==/UserScript==
